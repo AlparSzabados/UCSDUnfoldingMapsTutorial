759354eb0e1dcf5b1179302a8d18dae18d59bda9
alpar <szabadosalpar@gmail.com>
2016-11-11 15:18:02 +0200

    removed unnecessary comments and minor format.

--- src/main/module5/CommonMarker.java
+++ src/main/module5/CommonMarker.java
@@ -8,12 +8,11 @@ import processing.core.PGraphics;
  * Implements a common marker for cities and earthquakes on an earthquake map
  *
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Szabados Alpar
  */
 public abstract class CommonMarker extends SimplePointMarker {
 
-    // Records whether this marker has been clicked (most recently)
-    protected boolean clicked = false;
+    private boolean clicked = false;
 
     public CommonMarker(Location location) {
         super(location);
@@ -23,27 +22,18 @@ public abstract class CommonMarker extends SimplePointMarker {
         super(location, properties);
     }
 
-    // Getter method for clicked field
     public boolean getClicked() {
         return clicked;
     }
 
-    // Setter method for clicked field
     public void setClicked(boolean state) {
         clicked = state;
     }
 
-    // Common piece of drawing method for markers;
-    // Note that you should implement this by making calls
-    // drawMarker and showTitle, which are abstract methods
-    // implemented in subclasses
     public void draw(PGraphics pg, float x, float y) {
-        // For starter code just drawMaker(...)
         if (!hidden) {
             drawMarker(pg, x, y);
-            if (selected) {
-                showTitle(pg, x, y);  // You will implement this in the subclasses
-            }
+            if (selected) showTitle(pg, x, y);
         }
     }
 
